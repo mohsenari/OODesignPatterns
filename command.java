f7c9dcd8c66db96626cadfa2bac1839fc759e941
Mohsen Ansari <mansari@deloitte.ca>
2019-06-26 15:38:57 -0400

    added command design pattern

---
+++ command.java
@@ -0,0 +1,100 @@
+// invoker class to define execute and unexecute methods for commands
+class Invoker {
+    ICommand on;
+    ICommand dim;
+    public Invoker(ICommand on, ICommand dim) {
+        this.on = on;
+        this.dim = dim;
+    }
+
+    public void turnOnLights() {
+        this.on.execute();
+    }
+
+    public void turnOffLights() {
+        this.on.unexecute();
+    }
+
+    public void dimLights() {
+        this.dim.execute();
+    }
+
+    public void brightLights() {
+        this.dim.unexecute();
+    }
+}
+
+interface ICommand {
+    public void execute();
+    public void unexecute();
+}
+
+// first implementation of command class
+class LightOnCommand implements ICommand{
+    private Light light;
+
+    public LightOnCommand(Light light){
+        this.light = light;
+    }
+
+    public void execute(){
+        this.light.on();
+    }
+
+    public void unexecute() {
+        this.light.off();
+    }
+}
+
+// second implementation of command interface
+class LightDimCommand implements ICommand{
+    private Light light;
+
+    public LightDimCommand(Light light){
+        this.light = light;
+    }
+
+    public void execute(){
+        this.light.dim();
+    }
+
+    public void unexecute() {
+        this.light.bright();
+    }
+}
+
+// receiver class - command class controls this
+class Light {
+    private String status = "off";
+    public void on(){
+        this.status = "on";
+        System.out.println(this.status);
+    }
+
+    public void off(){
+        this.status = "off";
+        System.out.println(this.status);
+    }
+
+    public void dim() {
+        this.status = "dim";
+        System.out.println(this.status);
+    }
+
+    public void bright() {
+        this.status = "bright";
+        System.out.println(this.status);
+    }
+}
+
+// main class to instantiate the receiver (light) and the invoker with commands
+class Main {
+    public static void main (String args[]){
+        Light light = new Light();
+        Invoker invoker = new Invoker(new LightOnCommand(light), new LightDimCommand(light));
+        invoker.turnOnLights(); //on
+        invoker.dimLights(); //dim
+        invoker.brightLights(); //bright
+        invoker.turnOffLights(); //off
+    }
+}
