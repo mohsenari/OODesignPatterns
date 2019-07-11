722fc022adfc951e8103e7a7857df914e14eddd9
Mohsen Ansari <mansari@deloitte.ca>
2019-07-11 14:07:55 -0400

    added unit test for singleton

--- SingletonWithUnitTest.java
+++ SingletonWithUnitTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
 class Singleton {
     private static Singleton instance;
     private int random = 0;
@@ -33,3 +42,19 @@ class Main {
         System.out.println(y.getNumber());
     }
 }
+
+public class SingletonWithUnitTest {
+
+    @Test
+    public void testSingleton() {
+        Singleton one = Singleton.getInstance();
+        Singleton two = Singleton.getInstance();
+
+        // both instances should have the same object reference
+        assertEquals(one, two);
+
+        // Both instances should show the same random value
+        assertEquals(one.getNumber(), two.getNumber());
+    }
+}
+
